5f28db92de0cce8e1c8fef4970314c89db9dacbe
Nick Martin <nmartin867@gmail.com>
2013-11-06 19:40:26 -0700

    more work

--- examples/demo_service/service.js
+++ examples/demo_service/service.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+app.use(express.bodyParser());
 
-app.post('/users/save/:user', function(req, res){
+app.post('/users/save/', function(req, res){
   res.json({message: 'user saved successfully'})
 });
 
